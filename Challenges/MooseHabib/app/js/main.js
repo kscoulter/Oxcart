e7ee2a81722c32eeedaadb58fe46e33284c5c60e
Melissa Steffan <melissa.steffan@washpost.com>
2016-02-27 12:32:53 -0500

    add solution to Moose's challenge

--- Challenges/MooseHabib/app/js/main.js
+++ Challenges/MooseHabib/app/js/main.js
@@ -1,1 +1,17 @@
-// console.log('Main');
+function checkSum () {
+  var numArray = $(".num");
+  var total = 0;
+  for (var i=0; i < numArray.length; i++){
+    var number = parseInt($(numArray[i]).val())
+    if(isNaN(number)){
+      number = 0;
+    }
+    total = total + number;
+  }
+  $("#sum").append("<h3>The sum is: " + total + "</h3>")
+
+}
+
+$("#getSum").click(function(){
+  checkSum();
+})
--- Challenges/MooseHabib/app/scripts.js
+++ Challenges/MooseHabib/app/scripts.js
@@ -2,7 +2,22 @@
 
 // console.log('jquery');
 
-// console.log('Main');
+function checkSum () {
+  var numArray = $(".num");
+  var total = 0;
+  for (var i=0; i < numArray.length; i++){
+    var number = parseInt($(numArray[i]).val())
+    if(isNaN(number)){
+      number = 0;
+    }
+    total = total + number;
+  }
+  $("#sum").append("<h3>The sum is: " + total + "</h3>")
 
+}
+
+$("#getSum").click(function(){
+  checkSum();
+})
 
 //# sourceMappingURL=scripts.js.map
